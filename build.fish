2ea1255df9cfa1c5a50ed9fa0eceb38f482d688a
Nathan Chancellor <nathan@kernel.org>
2022-01-10 14:15:21 -0700

    build.fish: Hoist configuration manipulation into cbl_rb_pi
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- build.fish
+++ build.fish
@@ -22,56 +22,34 @@ set out .build/$arch
 set full_out $krnl_src/$out
 
 set common_make_args \
-   -C $krnl_src \
-   HOSTLDFLAGS=-fuse-ld=lld \
-   INSTALL_DTBS_PATH=rootfs \
-   INSTALL_MOD_PATH=rootfs \
-   KCFLAGS=-Werror \
-   LLVM=1 \
-   LLVM_IAS=1 \
-   O=$out
+    -C $krnl_src \
+    HOSTLDFLAGS=-fuse-ld=lld \
+    INSTALL_DTBS_PATH=rootfs \
+    INSTALL_MOD_PATH=rootfs \
+    KCFLAGS=-Werror \
+    LLVM=1 \
+    LLVM_IAS=1 \
+    O=$out
 
 # This is not cleaned up by 'distclean' so do it manually here
 rm -rf $out/rootfs
 
 switch $arch
     case arm
-        PO="$path_overrides" \
-           kmake \
-           ARCH=arm \
-           CROSS_COMPILE=arm-linux-gnueabi- \
-           $common_make_args \
-           distclean defconfig all dtbs_install modules_install; or exit
+        set arch_make_args ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
 
         set kernel_image zImage
 
     case arm64
         set arch_make_args ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
 
-        PO="$path_overrides" \
-           kmake \
-           $arch_make_args \
-           $common_make_args \
-           distclean defconfig; or exit
-
-        set fish_trace 1
-        $krnl_src/scripts/config \
-           --file $full_out/.config \
-           -d DEBUG_INFO \
-           -d LTO_NONE \
-           -e CFI_CLANG \
-           -e LTO_CLANG_THIN \
-           -e SHADOW_CALL_STACK
-        set -e fish_trace
-
-        PO="$path_overrides" \
-           kmake \
-           $arch_make_args \
-           $common_make_args \
-           olddefconfig all dtbs_install modules_install; or exit
-
         set kernel_image Image
 end
+
+PO="$path_overrides" kmake \
+    $arch_make_args \
+    $common_make_args \
+    distclean defconfig all dtbs_install_modules_install; or exit
 
 set kernel $full_out/arch/$arch/boot/$kernel_image
 if test -f $kernel
